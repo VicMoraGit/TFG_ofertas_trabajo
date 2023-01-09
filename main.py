5429d01298ca324db26bc7835704438ea145825a
VicMoraGit <victormoradj@hotmmail.com>
2023-01-09 12:13:35 +0100

    graficos

--- main.py
+++ main.py
@@ -11,7 +11,38 @@ from portales.monster.monster import Monster
 
 from portales.portal import Portal
 
+# Visualizacion de datos
+import pandas as pd
+import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter 
 
+
+raw_csv_file=open("data/CSV/Tecnoempleo.csv", 'r', encoding="UTF-8", newline="")
+
+df=pd.read_csv(raw_csv_file)
+
+df.drop(df[df['Experiencia'] == "Sin informacion"].index, inplace = True)
+
+años_experiencia=df['Experiencia']
+
+print(años_experiencia)
+Counter = Counter(años_experiencia) 
+most_occur = Counter.most_common(10) 
+
+
+fig = plt.figure(figsize = (10, 10)) 
+experiencia = pd.DataFrame(Counter.most_common(6) ,
+                             columns=['experiencia', 'count'])
+print(experiencia)
+# creating the bar plot 
+plt.bar(experiencia['experiencia'], experiencia['count'], color ='maroon',  
+        width = 0.4) 
+  
+plt.xlabel("Años") 
+plt.ylabel("Cantidad") 
+plt.title("Experiencia media Tecnoempleo") 
+plt.savefig('TecnoempleoExperiencia.png')
 
 
 if __name__ == "__main__":
