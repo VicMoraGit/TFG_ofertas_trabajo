efc6532d8f372542f843c99d316e26f3ca22a74a
VicMoraGit <victormoradj@hotmmail.com>
2023-05-09 13:54:43 +0200

    feat: ✨ Todas las subcategorias especificadas
    - Se han especificado todas las subcategorias

--- util/enums/categorias_enum.py
+++ util/enums/categorias_enum.py
@@ -3,10 +3,10 @@ from enum import Enum
 class Categoria(Enum):
     FRAMEWORK = "Framework"
     LIBRERIA = "Librería"
+    SDK = "Kit de desarrollo de software"
     SOFTWARE = "Software"
     MIDDLEWARE = "Middleware"
     LENGUAJE = "Lenguaje"
-    METODOLOGIA = "Metodología"
     IDE = "Entorno de desarrollo integrado"
     OS = "Sistema Operativo"
     ESPECIFICACION = "Especificacíon"
--- util/enums/subcategorias_enums.py
+++ util/enums/subcategorias_enums.py
@@ -13,42 +13,62 @@ class SubFramework(Enum):
     ROBOTICA = "Robótica"
     BLOCKCHAIN = "Cadena de bloques"
 class SubLibreria(Enum):
-    pass
-
+    GESTION_DATOS = "Gestión de datos"
+    UI = "Interfaz de usuario"
+    AI = "Inteligencia Artificial"
+    ACCESS = "Accesibilidad"
+    DEPENDENCIAS = "Dependencias"
+    DOM = "Manipulacion DOM"
+    IO = "Comunicacion C/S"
+    ESTADO = "Gestión de estado"
+    API = "Contenedor de API"
+    TEST = "Testing"
+    pass    
 class SubSoftware(Enum):
+    CMS = "Sistema de gestor de contenidos"
+    DISENYO = "Diseño"
+    BUILD = "Construcción de proyectos"
+    TEST = "Testing"
+    NEGOCIOS = "Negocios"
+    LDAP = "Acceso a directorio"
+    VIRTUAL = "Virtualización"
+    CONT = "Orquestación y contenedores"
+    MENSAJERIA = "Comunicación y mensajería"
+    OFFICE = "Ofimática"
+    RENDIMIENTO = "Rendimiento y monitorización"
+    RED = "Redes"
+    SEG = "Seguridad"
+    ECM = "Gestión documental empresarial"
+    SUITE = "Suite de servicios en la nube"
+    CICD = "Integración continua y entrega continua"
+    ERP = "Sistema de planificación de recursos empresariales"
+    SEGUIMIENTO = "Seguimiento de proyectos e incidencias"
+    CRM = "Gestión de relaciones con clientes"
     pass
 
-class SubMiddleware(Enum):
-    pass
-
-class SubLenguaje(Enum):
-    pass
-
-class SubMetodologia(Enum):
-    pass
-
-class SubIDE(Enum):
-    pass
+class SubNoAplica(Enum):
+    NO_APLICA = "No aplica"
 
 class SubOS(Enum):
+    LINUX = "GNU/Linux"
+    WIN = "Windows"
+    BSD = "BSD (Berkeley Software Distribution)"
+    UNIX = "UNIX"
     pass
 
-class SubEspecificacion(Enum):
-    pass
 
 class SubServidor(Enum):
-    pass
-
-class SubEE(Enum):
+    WEB ="Web"
     pass
 
 class SubSGBD(Enum):
-    pass
-
-class SubCLI(Enum):
+    SQL = "Base de datos relacional",
+    NO_SQL = "Base de datos no relacional"
     pass
 
 class SubVCS(Enum):
+    DIST = "Distribuido",
+    CENT = "Centralizado"
     pass
 
 
